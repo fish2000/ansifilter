bb6debc5e80a39f01fe38a68cf5c00165f95580b
Andre Simon <as@andre-simon.de>
2019-10-30 20:58:22 +0100

    fixed help message

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ void ANSIFilterApp::printHelp()
     cout << "\nFormat options:\n";
     cout << "  -a, --anchors(=self)   Add HTML line anchors (opt: self referencing, assumes -l)\n";
     cout << "  -d, --doc-title        Set HTML/LaTeX/SVG document title\n";
-    cout << "  -e, --encoding=<name>  Set HTML/RTF encoding (must match input file encoding);\n"; 
+    cout << "  -e, --encoding=<enc>   Set HTML/RTF encoding (must match input file encoding);\n"; 
     cout << "                         omit encoding information if enc=NONE\n";
     cout << "  -f, --fragment         Omit HTML header and footer\n";
     cout << "  -F, --font=<font>      Set HTML/RTF/SVG font face\n";
